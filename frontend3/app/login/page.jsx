c59fe31a60a5b5d60290042bfe7dcf129b878a42
Abdul Rehman Khan <khan456.tdm@gmail.com>
2025-06-05 10:35:36 +0500

    [🛠️]

--- frontend3/app/login/page.jsx
+++ frontend3/app/login/page.jsx
@@ -1,13 +1,22 @@
 'use client'
 import LoginForm from '@/components/LoginForm'
+import useAuthStore from '@/store/useAuthStore'
 import { useRouter } from 'next/navigation'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function LoginPage() {
-  const [form, setForm] = useState({ username: '', password: '' })
+  const user = useAuthStore(state => state.user);
   const router = useRouter()
 
-
+  useEffect(() => {
+    if (user) {
+      if(user?.role == "student"){
+        router.replace('/dashboard');
+      } else {
+        router.replace('/admin/dashboard');
+      }
+    }
+  },[])
 
   return (
     <main className="flex items-center justify-center min-h-screen bg-gradient-to-b from-gray-900 to-emerald-700 bg-opacity-70 backdrop-blur-sm">
